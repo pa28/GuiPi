03db6794dfa2f1fcf657fd1230c08d4137fddb74
richard <richard.buckley@ieee.org>
2020-10-05 16:03:58 -0400

    Add another solar image.

--- resources/images/fetch_images.sh
+++ resources/images/fetch_images.sh
@@ -7,3 +7,4 @@ curl https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_0193.jpg --output "A
 echo "latest_512_211193171.jpg"
 curl https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_211193171.jpg --output "AIA 211 Å, 193 Å, 171 Å.jpg"
 curl https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_HMIB.jpg --output "HMI Magnetogram.jpg"
+curl https://sdo.gsfc.nasa.gov/assets/img/latest/latest_512_HMIIC.jpg --output "HMI Intensitygram.jpg"
